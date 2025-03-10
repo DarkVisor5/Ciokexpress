c329976fde5159471047f15382ea6ceceea3313b
Leonardo Lanaia <l.lvisor@hotmail.it>
2025-03-10 11:46:59 +0100

    Css updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://DarkVisor5.github.io/Ciokexpress/',
+  base: '/Ciokexpress/',  // Fixes GitHub Pages paths
   integrations: [tailwind()],
+  build: {
+    format: 'directory', // Ensures correct asset linking
+  }
 });
